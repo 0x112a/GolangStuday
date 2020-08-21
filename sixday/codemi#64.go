67d49604155da9c126b46c6366fb27f4ab68e510
0x112 <1436063244@qq.com>
2020-08-21 13:26:40 +0800

    modify some error

--- sixday/codemi#64.go
+++ sixday/codemi#64.go
@@ -1,19 +1,15 @@
 // code.mi.com #64
 //描述
 //有 500 个小孩围成一圈，编号从 1 到 500，从第一个开始报数：1，2，3，1，2，3，1，2，3，……每次报到 3 的小孩退出。问第 n 个被淘汰的小孩，在最开始 500 人里是的编号是几？
-//
 //输入
 //正整数 N，表示要计算的为第 N 个淘汰的小孩的编号，0 < N <= 500
-//
 //输出
 //第 N 个淘汰的小孩的编号
-//
 //输入样例
 //1
 //2
 //206
 //311
-//复制样例
 //输出样例
 //3
 //6
@@ -30,7 +26,6 @@ import (
 )
 
 func solution(line string) string {
-	// please write your code here
 	N, _ := strconv.Atoi(line)
 	var aws string
 	var cnt int
@@ -53,9 +48,6 @@ func solution(line string) string {
 		}
 	}
 	return aws
-	//fmt.Println(index)
-	// 返回处理后的结果
-	// return ans
 }
 
 func main() {
